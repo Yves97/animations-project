1208a0107f57e9a61775143d485fd540603ed6f3
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-03-07 15:24:03 +0000

    footer end2

--- src/App.vue
+++ src/App.vue
@@ -324,25 +324,6 @@
               </ul>
             </div>
           </div>
-          <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 col-xl-3">
-            <div class="footer-content">
-              <p>Yves Product</p>
-              <ul>
-                <li>
-                  <a href="#">Web design</a>
-                </li>
-                <li>
-                  <a href="#">Graphic design</a>
-                </li>
-                <li>
-                  <a href="#">Front end Developper</a>
-                </li>
-                <li>
-                  <a href="#">Ressources</a>
-                </li>
-              </ul>
-            </div>
-          </div>
         </div>
       </div>
     </footer>
