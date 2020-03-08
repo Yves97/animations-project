31c7f4b7b6dbf4e091f5f105325ab8196a23b2cf
yvnan2.19 <yveskouadio111@yahoo.fr>
2020-03-08 00:33:52 +0000

    fix components

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 
 import About from './components/About.vue'
 import Portfolio from './components/Portfolio.vue'
+import Home from './components/Home.vue'
 
 Vue.component('app-about',About)
 Vue.component('app-portfolio',Portfolio)
+Vue.component('app-home',Home)
 Vue.config.productionTip = false
 
 new Vue({
